81adb3ccf2ad38f67e070882872a86e6d49c1d15
TienNM99 <tiennm99@outlook.com>
2022-04-20 23:01:33 +0700

    [Add] build

---
+++ src/settings.2e1d7.js
@@ -0,0 +1,24 @@
+window._CCSettings = {
+    platform: "web-desktop",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    hasResourcesBundle: true,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/Scene/Initializer.fire",
+    orientation: "",
+    debug: true,
+    jsList: [],
+    bundleVers: {
+        internal: "d36c4",
+        resources: "9b439",
+        main: "5834e"
+    }
+};
