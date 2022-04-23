714168764e9c87c25763d7332a4924ddab3a457c
TienNM99 <tiennm99@outlook.com>
2022-04-23 20:29:01 +0700

    [Build] test ssl

---
+++ src/settings.c4eb2.js
@@ -0,0 +1,24 @@
+window._CCSettings = {
+    platform: "web-desktop",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    hasResourcesBundle: true,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/Scene/Initializer.fire",
+    orientation: "",
+    debug: true,
+    jsList: [],
+    bundleVers: {
+        internal: "cb4f5",
+        resources: "fdc97",
+        main: "39e39"
+    }
+};
